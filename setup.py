f8d6a7f3c53bf30bb1660355fad916c82f32c175
bradleyfay <bradley.fay@gmail.com>
2015-03-16 17:15:09 -0700

    added package details

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='py-goldsberry',
+      version='0.0',
+      description='Easy collection of NBA Statistics',
+      url='http://github.com/bradleyfay/py-goldsberry',
+      author='Bradley Fay',
+      author_email='bradley.fay@gmail.com',
+      license='MIT',
+      packages=['goldsberry'],
+      zip_safe=False)
