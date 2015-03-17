4ea0c1a6e557810b49225cafbe3f116c7d87a132
bradleyfay <bradley.fay@gmail.com>
2015-03-16 17:38:13 -0700

    additional header info

--- setup.py
+++ setup.py
@@ -3,9 +3,15 @@ from setuptools import setup
 setup(name='py-goldsberry',
       version='0.0',
       description='Easy collection of NBA Statistics',
+      long_description='Really, the funniest around.',
       url='http://github.com/bradleyfay/py-goldsberry',
       author='Bradley Fay',
       author_email='bradley.fay@gmail.com',
       license='MIT',
+      keywords='funniest joke comedy flying circus',
       packages=['goldsberry'],
+      install_requires=[
+          'requests',
+      ],
+      include_package_data=True,
       zip_safe=False)
