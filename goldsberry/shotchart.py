5c791df2c8c29fc8c7a3b8dd8c93103dce97945a
Bradley Fay <bradley.fay@gmail.com>
2015-05-06 17:59:19 -0700

    updated to new code schema

--- goldsberry/shotchart.py
+++ goldsberry/shotchart.py
@@ -1,7 +1,8 @@
 class ShotChart:
     def __init__(self,playerid,leagueid='',season='2013-14', seasontype='Regular Season',teamid=0,gameid='',outcome='',location='',
-              month=0,seasonsegment='',datefrom='',dateto='',opponentteamid=0,vsconf='',vsdiv='',position='',gamesegment='',
-              period=0,lastngames=0,aheadbehind='',contextmeasure='FGM',clutchtime='',rookieyear=''):
+              month=0,seasonsegment='',datefrom='',dateto='',opponentteamid=0,vsconf='',vsdiv='',position='',
+              period=0,lastngames=0,aheadbehind='',contextmeasure='FGM',clutchtime='',rookieyear='', contextfilter='',
+              startperiod='1',endperiod='10',startrange='0', endrange='28800', gamesegment='', rangetype='2'):
         self._url = "http://stats.nba.com/stats/shotchartdetail?"
         self._api_param = {
              'LeagueID': leagueid,
@@ -26,11 +27,20 @@ class ShotChart:
              'AheadBehind' : aheadbehind,
              'ContextMeasure' : contextmeasure,
              'ClutchTime' : clutchtime,
-             'RookieYear' : rookieyear
+             'RookieYear' : rookieyear,
+             'ContextFilter':contextfilter,
+             'StartPeriod':startperiod,
+             'EndPeriod':endperiod,
+             'StartRange':startrange,
+             'EndRange':endrange,
+             'RangeType':rangetype,
              }
-        self._x = requests.get(self._url, params=self._api_param)
-        self._x = self._x.json()
+        self._pull = requests.get(self._url, params=self._api_param)
     def shotchart(self):
-        return self._x['resultSets'][0]['rowSet'],columns=self._x['resultSets'][0]['headers']
+        _headers = self._pull.json()['resultSets'][0]['headers']
+        _values = self._pull.json()['resultSets'][0]['rowSet']
+        return [dict(zip(_headers, value)) for value in _values]
     def leagueaverage(self):
-        return self._x['resultSets'][1]['rowSet'],columns=self._x['resultSets'][1]['headers']
+        _headers = self._pull.json()['resultSets'][1]['headers']
+        _values = self._pull.json()['resultSets'][1]['rowSet']
+        return [dict(zip(_headers, value)) for value in _values]
