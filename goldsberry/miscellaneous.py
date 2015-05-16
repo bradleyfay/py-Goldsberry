a10f744f4345fa2a75247a5cfebfb9cfe51d4b43
Bradley Fay <bradley.fay@gmail.com>
2015-05-16 15:36:58 -0700

    added playoff picture and franchise history

--- goldsberry/miscellaneous.py
+++ goldsberry/miscellaneous.py
@@ -4,7 +4,7 @@ class Transactions:
     def __init__(self):
         self._url = "http://stats.nba.com/feeds/NBAPlayerTransactions-559107/json.js"
         self._pull = requests.get(self._url)
-    def data(self):
+    def Transactions(self):
         return self._pull.json()['ListItems']
 
 class DailyStandings:
@@ -57,7 +57,58 @@ class PlayerList:
                            'Season': Season
         }
         self._pull = requests.get(self._url, params=self._api_param)
-    def data(self):
+    def Players(self):
         _headers = self._pull.json()['resultSets'][0]['headers']
         _values = self._pull.json()['resultSets'][0]['rowSet']
+        return [dict(zip(_headers, value)) for value in _values]
+
+class FranchiseHistory:
+    """
+    """
+    def __init__(self, LeagueID='00'):
+        self._url = "http://stats.nba.com/stats/franchisehistory?"
+        self._api_param = {'LeagueID':LeagueID
+                           }
+        self._pull = requests.get(self._url, params=self._api_param)
+    def FranchiseHistory(self):
+        _headers = self._pull.json()['resultSets'][0]['headers']
+        _values = self._pull.json()['resultSets'][0]['rowSet']
+        return [dict(zip(_headers, value)) for value in _values]
+    def DefunctTeams(self):
+        _headers = self._pull.json()['resultSets'][1]['headers']
+        _values = self._pull.json()['resultSets'][1]['rowSet']
+        return [dict(zip(_headers, value)) for value in _values]
+
+class PlayoffPicture:
+    """
+    """
+    def __init__(self, LeagueID='00', SeasonID='22014'):
+        self._url = "http://stats.nba.com/stats/franchisehistory?"
+        self._api_param = {'LeagueID':LeagueID,
+                           'SeasonID':SeasonID
+                           }
+        self._pull = requests.get(self._url, params=self._api_param)
+    def EastConfPlayoffPicture(self):
+        _headers = self._pull.json()['resultSets'][0]['headers']
+        _values = self._pull.json()['resultSets'][0]['rowSet']
+        return [dict(zip(_headers, value)) for value in _values]
+    def WestConfPlayoffPicture(self):
+        _headers = self._pull.json()['resultSets'][1]['headers']
+        _values = self._pull.json()['resultSets'][1]['rowSet']
+        return [dict(zip(_headers, value)) for value in _values]
+    def EastConfStandings(self):
+        _headers = self._pull.json()['resultSets'][2]['headers']
+        _values = self._pull.json()['resultSets'][2]['rowSet']
+        return [dict(zip(_headers, value)) for value in _values]
+    def WestConfStandings(self):
+        _headers = self._pull.json()['resultSets'][3]['headers']
+        _values = self._pull.json()['resultSets'][3]['rowSet']
+        return [dict(zip(_headers, value)) for value in _values]
+    def EastConfRemainingGames(self):
+        _headers = self._pull.json()['resultSets'][4]['headers']
+        _values = self._pull.json()['resultSets'][4]['rowSet']
+        return [dict(zip(_headers, value)) for value in _values]
+    def WestConfRemainingGames(self):
+        _headers = self._pull.json()['resultSets'][5]['headers']
+        _values = self._pull.json()['resultSets'][5]['rowSet']
         return [dict(zip(_headers, value)) for value in _values]
