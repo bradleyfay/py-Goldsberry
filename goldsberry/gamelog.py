a8b1100342dd6a9282e74e3aba483697cf7cf6e1
Bradley Fay <bradley.fay@gmail.com>
2015-07-03 14:30:41 -0400

    updated gamelog.py to reflect new data pull model and nba api

--- goldsberry/gamelog.py
+++ goldsberry/gamelog.py
@@ -1,12 +1,13 @@
 class GameLog:
-    def __init__(self, playerid, season='2013-14',seasontype='Regular Season', leagueid=''):
+    def __init__(self, playerid, season='2014-15',seasontype='Regular Season', leagueid='00'):
         self._url = "http://stats.nba.com/stats/playergamelog?"
         self._api_param = {'PlayerID':playerid,
                           'SeasonType': seasontype,
                           'Season': season,
                           'LeagueID': leagueid
                           }
-        self._x = requests.get(self._url, params=self._api_param)
-        self._x = self._x.json()
-    def log(self):
-        return self._x['resultSets'][0]['rowSet'],columns=self._x['resultSets'][0]['headers']
+        self._pull = requests.get(self._url, params=self._api_param)
+    def Log(self):
+        _headers = self._pull.json()['resultSets'][0]['headers']
+        _values = self._pull.json()['resultSets'][0]['rowSet']
+        return [dict(zip(_headers, value)) for value in _values]
