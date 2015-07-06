fe0e8ac4e254ef1f943a114922d680c9d8367c88
Bradley Fay <bradley.fay@gmail.com>
2015-07-06 12:18:25 -0400

    added additional tables to playerlog. need to add .js based data in 2.0

--- goldsberry/playerlog.py
+++ goldsberry/playerlog.py
@@ -72,4 +72,60 @@ class Splits:
 
 measuretype = "(Base)|(Advanced)|(Misc)|(Four Factors)|(Scoring)|(Opponent)|(Usage)"
 
+class Career:
+    def __init__(self, playerid, leagueid='00',permode="PerGame"):
+      self._url = "http://stats.nba.com/stats/playercareerstats?"
+      self._api_param = {'PlayerID':playerid,
+                         'LeagueID':leagueid,
+                         'PerMode':permode}
+      self._pull = requests.get(self.url, params = self._api_param)
+    def SeasonTotalsRegularSeason(self):
+        _headers = self._pull.json()['resultSets'][0]['headers']
+        _values = self._pull.json()['resultSets'][0]['rowSet']
+        return [dict(zip(_headers, value)) for value in _values]
+    def CareerTotalsRegularSeason(self):
+        _headers = self._pull.json()['resultSets'][1]['headers']
+        _values = self._pull.json()['resultSets'][1]['rowSet']
+        return [dict(zip(_headers, value)) for value in _values]
+    def SeasonTotalsPostSeason(self):
+        _headers = self._pull.json()['resultSets'][2]['headers']
+        _values = self._pull.json()['resultSets'][2]['rowSet']
+        return [dict(zip(_headers, value)) for value in _values]
+    def CareerTotalsPostSeason(self):
+        _headers = self._pull.json()['resultSets'][3]['headers']
+        _values = self._pull.json()['resultSets'][3]['rowSet']
+        return [dict(zip(_headers, value)) for value in _values]
+    def SeasonTotalsAllStarSeason(self):
+        _headers = self._pull.json()['resultSets'][4]['headers']
+        _values = self._pull.json()['resultSets'][4]['rowSet']
+        return [dict(zip(_headers, value)) for value in _values]
+    def CareerTotalsAllStarSeason(self):
+        _headers = self._pull.json()['resultSets'][5]['headers']
+        _values = self._pull.json()['resultSets'][5]['rowSet']
+        return [dict(zip(_headers, value)) for value in _values]
+    def SeasonTotalsCollegeSeason(self):
+        _headers = self._pull.json()['resultSets'][6]['headers']
+        _values = self._pull.json()['resultSets'][6]['rowSet']
+        return [dict(zip(_headers, value)) for value in _values]
+    def CareerTotalsCollegeSeason(self):
+        _headers = self._pull.json()['resultSets'][7]['headers']
+        _values = self._pull.json()['resultSets'][7]['rowSet']
+        return [dict(zip(_headers, value)) for value in _values]
+    def SeasonRankingsRegularSeason(self):
+        _headers = self._pull.json()['resultSets'][8]['headers']
+        _values = self._pull.json()['resultSets'][8]['rowSet']
+        return [dict(zip(_headers, value)) for value in _values]
+    def SeasonRankingsPostSeason(self):
+        _headers = self._pull.json()['resultSets'][9]['headers']
+        _values = self._pull.json()['resultSets'][9]['rowSet']
+        return [dict(zip(_headers, value)) for value in _values]
 
+class Demo:
+    def __init__(self, playerid):
+      self._url = "http://stats.nba.com/stats/commonplayerinfo?"
+      self._api_param = {'PlayerID':playerid}
+      self._pull = requests.get(self.url, params = self._api_param)
+    def CommonPlayerInfo(self):
+        _headers = self._pull.json()['resultSets'][0]['headers']
+        _values = self._pull.json()['resultSets'][0]['rowSet']
+        return [dict(zip(_headers, value)) for value in _values]
