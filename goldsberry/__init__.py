2b9279b071e5f940bed9698f27b6249ac988be85
Bradley Fay <bradley.fay@gmail.com>
2015-07-16 21:27:09 -0400

    updated version number to 3.5

--- goldsberry/__init__.py
+++ goldsberry/__init__.py
@@ -1,6 +1,6 @@
 __author__ = 'Bradley Fay'
 __email__ = 'bradley.fay@gmail.com'
-__version__ = '0.3.4'
+__version__ = '0.3.5'
 
 import requests as _requests
 from goldsberry._apiFunc import _nbaSeason, _nbaLeague
