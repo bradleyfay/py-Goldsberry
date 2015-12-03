9b60355ce5bd567b5b45ebbe6fbf47949a4d1f42
Bradley Fay <bradley.fay@gmail.com>
2015-12-02 21:08:01 -0500

    bug fix and version push

--- goldsberry/__init__.py
+++ goldsberry/__init__.py
@@ -1,6 +1,6 @@
 __author__ = 'Bradley Fay'
 __email__ = 'bradley.fay@gmail.com'
-__version__ = '0.6.3'
+__version__ = '0.6.3.1'
 
 import requests as _requests
 from goldsberry._apiFunc import *
--- goldsberry/_apiFunc.py
+++ goldsberry/_apiFunc.py
@@ -25,7 +25,7 @@ def _nbaSeason(x):
     """
     if len(str(x)) == 4:
         try:
-            return '{0}-{1}'.format(y, str(int(y) % 100 + 1)[-2:].zfill(2))
+            return '{0}-{1}'.format(x, str(int(x) % 100 + 1)[-2:].zfill(2))
         except ValueError: 
             raise ValueError("Enter the four digit year for the first half of the desired season")
     else: raise ValueError("Enter the four digit year for the first half of the desired season")
