66d986cdfd9d6f5e2968924cf4a4ad38cb5dd2c7
Bradley Fay <bradley.fay@gmail.com>
2016-02-07 15:22:34 -0500

    Fixed bug in parameter inheritance problem

--- goldsberry/game/_Game2.py
+++ goldsberry/game/_Game2.py
@@ -1,15 +1,23 @@
 from goldsberry._masterclass import *
 from goldsberry._apiparams import *
 
-class play_by_play(GAME, default_parameters):
-    _url_modifier = 'playbyplayv2'
+class play_by_play(NBA_datapull):
+    def __init__(self, gameid):
+        NBA_datapull.__init__(self)
+        self.SET_parameters(GameID = gameid, **p_base)
+        self._url_modifier = 'playbyplayv2'
+        self.GET_raw_data()
     def player_info(self):
         return self._get_table_from_data(self._datatables, 0)
     def _available_video(self):
         return self._get_table_from_data(self._datatables, 1)
 
-class boxscore_summary(GAME, default_parameters):
-    _url_modifier = 'boxscoresummaryv2'
+class boxscore_summary(NBA_datapull):
+    def __init__(self, gameid):
+        NBA_datapull.__init__(self)
+        self.SET_parameters(GameID = gameid, **p_base)
+        self._url_modifier = 'boxscoresummaryv2'
+        self.GET_raw_data()
     def game_summary(self):
         return self._get_table_from_data(self._datatables, 0)
     def other_stats(self):
@@ -29,58 +37,90 @@ class boxscore_summary(GAME, default_parameters):
     def _available_video(self):
         return self._get_table_from_data(self._datatables, 8)
 
-class boxscore_traditional(GAME, default_parameters):
-    _url_modifier = 'boxscoretraditionalv2'
+class boxscore_traditional(NBA_datapull):
+    def __init__(self, gameid):
+        NBA_datapull.__init__(self)
+        self.SET_parameters(GameID = gameid, **p_base)
+        self._url_modifier = 'boxscoretraditionalv2'
+        self.GET_raw_data()
     def player_stats(self):
         return self._get_table_from_data(self._datatables, 0)
     def team_stats(self):
         return self._get_table_from_data(self._datatables, 1)
 
-class boxscore_advanced(GAME, default_parameters):
-    _url_modifier = 'boxscoreadvancedv2'
+class boxscore_advanced(NBA_datapull):
+    def __init__(self, gameid):
+        NBA_datapull.__init__(self)
+        self.SET_parameters(GameID = gameid, **p_base)
+        self._url_modifier = 'boxscoreadvancedv2'
+        self.GET_raw_data()
     def player_stats(self):
         return self._get_table_from_data(self._datatables, 0)
     def team_stats(self):
         return self._get_table_from_data(self._datatables, 1)
 
-class boxscore_miscellaneous(GAME, default_parameters):
-    _url_modifier = 'boxscoremiscv2'
+class boxscore_miscellaneous(NBA_datapull):
+    def __init__(self, gameid):
+        NBA_datapull.__init__(self)
+        self.SET_parameters(GameID = gameid, **p_base)
+        self._url_modifier = 'boxscoremiscv2'
+        self.GET_raw_data()
     def player_stats(self):
         return self._get_table_from_data(self._datatables, 0)
     def team_stats(self):
         return self._get_table_from_data(self._datatables, 1)
 
-class boxscore_scoring(GAME, default_parameters):
-    _url_modifier = 'boxscorescoringv2'
+class boxscore_scoring(NBA_datapull):
+    def __init__(self, gameid):
+        NBA_datapull.__init__(self)
+        self.SET_parameters(GameID = gameid, **p_base)
+        self._url_modifier = 'boxscorescoringv2'
+        self.GET_raw_data()
     def player_stats(self):
         return self._get_table_from_data(self._datatables, 0)
     def team_stats(self):
         return self._get_table_from_data(self._datatables, 1)
 
-class boxscore_fourfactors(GAME, default_parameters):
-    _url_modifier = 'boxscorefourfactorsv2'
+class boxscore_fourfactors(NBA_datapull):
+    def __init__(self, gameid):
+        NBA_datapull.__init__(self)
+        self.SET_parameters(GameID = gameid, **p_base)
+        self._url_modifier = 'boxscorefourfactorsv2'
+        self.GET_raw_data()
     def player_stats(self):
         return self._get_table_from_data(self._datatables, 0)
     def team_stats(self):
         return self._get_table_from_data(self._datatables, 1)
 
-class boxscore_usage(GAME, default_parameters):
-    _url_modifier = 'boxscoreusagev2'
+class boxscore_usage(NBA_datapull):
+    def __init__(self, gameid):
+        NBA_datapull.__init__(self)
+        self.SET_parameters(GameID = gameid, **p_base)
+        self._url_modifier = 'boxscoreusagev2'
+        self.GET_raw_data()
     def player_stats(self):
         return self._get_table_from_data(self._datatables, 0)
     def team_stats(self):
         return self._get_table_from_data(self._datatables, 1)
 
-class boxscore_tracking(GAME, default_parameters):
-    _url_modifier = 'boxscoreplayertrackv2'
+class boxscore_tracking(NBA_datapull):
+    def __init__(self, gameid):
+        NBA_datapull.__init__(self)
+        self.SET_parameters(GameID = gameid, **p_base)
+        self._url_modifier = 'boxscoreplayertrackv2'
+        self.GET_raw_data()
     def player_stats(self):
         return self._get_table_from_data(self._datatables, 0)
     def team_stats(self):
         return self._get_table_from_data(self._datatables, 1)
 
 # Courtesy of daniel.silvis@gmail.com
-class GameIDs(LEAGUE,game_id_params):
-    _url_modifier = 'leaguegamelog'
+class GameIDs(NBA_datapull):
+    def __init__(self):
+        NBA_datapull.__init__(self)
+        self.SET_parameters(**p_game_ids)
+        self._url_modifier = 'leaguegamelog'
+        self.GET_raw_data()
     def game_list(self):
         return self._get_table_from_data(self._datatables, 0)
 
