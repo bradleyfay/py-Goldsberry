69180b24286f9e6adf824c114b2dfc7d6bc40c9d
Bradley Fay <bradley.fay@gmail.com>
2016-02-07 15:34:52 -0500

    Fixed parameter inheritance issue

--- goldsberry/league/_Draft2.py
+++ goldsberry/league/_Draft2.py
@@ -2,27 +2,42 @@ from goldsberry._masterclass import *
 from goldsberry._apiparams import *
 
 class anthro(NBA_datapull):
-    _url_modifier = 'draftcombineplayeranthro'
+    def __init__(self):
+        NBA_datapull.__init__(self)
+        self.SET_parameters(**p_game_ids)
+        self._url_modifier = 'draftcombineplayeranthro'
     def data(self):
         return self._get_table_from_data(self._datatables, 0)
 
 class agility(NBA_datapull):
-    _url_modifier = 'draftcombinedrillresults'
+    def __init__(self):
+        NBA_datapull.__init__(self)
+        self.SET_parameters(**p_game_ids)
+        self._url_modifier = 'draftcombinedrillresults'
     def data(self):
         return self._get_table_from_data(self._datatables, 0)
 
 class non_stationary_shooting(NBA_datapull):
-    _url_modifier = 'draftcombinenonstationaryshooting'
+    def __init__(self):
+        NBA_datapull.__init__(self)
+        self.SET_parameters(**p_game_ids)
+        self._url_modifier = 'draftcombinenonstationaryshooting'
     def data(self):
         return self._get_table_from_data(self._datatables, 0)
 
 class spot_up_shooting(NBA_datapull):
-    _url_modifier = 'draftcombinespotshooting'
+    def __init__(self):
+        NBA_datapull.__init__(self)
+        self.SET_parameters(**p_game_ids)
+        self._url_modifier = 'draftcombinespotshooting'
     def data(self):
         return self._get_table_from_data(self._datatables, 0)
 
 class combine(NBA_datapull):
-    _url_modifier = 'draftcombinestats'
+    def __init__(self):
+        NBA_datapull.__init__(self)
+        self.SET_parameters(**p_game_ids)
+        self._url_modifier = 'draftcombinestats'
     def data(self):
         return self._get_table_from_data(self._datatables, 0)
 
