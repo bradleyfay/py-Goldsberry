a7783c41d874e9c802920b42b5d8ca88b96e37be
Bradley Fay <bradley.fay@gmail.com>
2016-04-19 22:12:34 -0400

    fix sportvu.py

--- goldsberry/sportvu/sportvu.py
+++ goldsberry/sportvu/sportvu.py
@@ -4,40 +4,56 @@ from goldsberry.masterclass import SportVuProvider
 class catch_and_shoot(SportVuProvider):
     def __init__(self, **kwargs):
         url_modifier = 'catchShoot'
-        SportVuProvider.__init__(self, url_modifier=url_modifier,**kwargs)
+        SportVuProvider.__init__(self, url_modifier=url_modifier, **kwargs)
 
 
 class defense(SportVuProvider):
-    _url_modifier = 'defense'
-
+    '''Sportvu specific data. Season totals'''
+    def __init__(self, **kwargs):
+        url_modifier = 'defense'
+        SportVuProvider.__init__(self, url_modifier=url_modifier, **kwargs)
 
 class drives(SportVuProvider):
-    _url_modifier = 'drives'
-
+    '''Sportvu specific data. Season totals'''
+    def __init__(self, **kwargs):
+        url_modifier = 'drives'
+        SportVuProvider.__init__(self, url_modifier=url_modifier, **kwargs)
 
 class passing(SportVuProvider):
-    _url_modifier = 'passing'
-
+    '''Sportvu specific data. Season totals'''
+    def __init__(self, **kwargs):
+        url_modifier = 'passing'
+        SportVuProvider.__init__(self, url_modifier=url_modifier, **kwargs)
 
 class touches(SportVuProvider):
-    _url_modifier = 'touches'
-
+    '''Sportvu specific data. Season totals'''
+    def __init__(self, **kwargs):
+        url_modifier = 'touches'
+        SportVuProvider.__init__(self, url_modifier=url_modifier, **kwargs)
 
 class pull_up_shooting(SportVuProvider):
-    _url_modifier = 'pullUpShoot'
-
+    '''Sportvu specific data. Season totals'''
+    def __init__(self, **kwargs):
+        url_modifier = 'pullUpShoot'
+        SportVuProvider.__init__(self, url_modifier=url_modifier, **kwargs)
 
 class rebounding(SportVuProvider):
-    _url_modifier = 'rebounding'
-
+    '''Sportvu specific data. Season totals'''
+    def __init__(self, **kwargs):
+        url_modifier = 'rebounding'
+        SportVuProvider.__init__(self, url_modifier=url_modifier, **kwargs)
 
 class shooting(SportVuProvider):
-    _url_modifier = 'shooting'
-
+    '''Sportvu specific data. Season totals'''
+    def __init__(self, **kwargs):
+        url_modifier = 'shooting'
+        SportVuProvider.__init__(self, url_modifier=url_modifier, **kwargs)
 
 class speed(SportVuProvider):
-    _url_modifier = 'speed'
-
+    '''Sportvu specific data. Season totals'''
+    def __init__(self, **kwargs):
+        url_modifier = 'speed'
+        SportVuProvider.__init__(self, url_modifier=url_modifier, **kwargs)
 
 __all__ = ['catch_and_shoot', 'defense', 'drives', 'passing',
            'touches', 'pull_up_shooting', 'rebounding', 'shooting',
