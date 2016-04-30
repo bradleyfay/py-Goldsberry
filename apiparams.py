7f285bd1bcdb9c0b6564d22c5f023fb31c419ef0
omer mazig <omermazig@hotmail.com>
2016-05-01 01:05:48 +0300

    *changed team's splits to return advanced stats by default - more interesting when it's not the same number of games *Little fix to masterclass

--- apiparams.py
+++ apiparams.py
@@ -233,7 +233,7 @@ p_team_split = {
     'LastNGames': 0,
     'LeagueID': default_league_id,
     'Location': '',
-    'MeasureType': 'Base',
+    'MeasureType': 'Advanced',
     'Month': 0,
     'OpponentTeamID': 0,
     'Outcome': '',
