55e608adec94969a99137d10f9b4481449ea711a
omer mazig <omermazig@hotmail.com>
2017-03-17 14:57:03 +0200

    * Added 'season' param to draft data

--- draft/_Draft2.py
+++ draft/_Draft2.py
@@ -3,45 +3,45 @@ from goldsberry.apiparams import *
 
 
 class anthro(NbaDataProvider):
-    def __init__(self):
+    def __init__(self, season=default_season):
         url_modifier = 'draftcombineplayeranthro'
-        NbaDataProvider.__init__(self, url_modifier=url_modifier, default_params=p_draft)
+        NbaDataProvider.__init__(self, url_modifier=url_modifier, default_params=p_draft, SeasonYear=season)
 
     def data(self):
         return self.object_manager.get_table_from_data(self.object_manager.data_tables, 0)
 
 
 class agility(NbaDataProvider):
-    def __init__(self):
+    def __init__(self, season=default_season):
         url_modifier = 'draftcombinedrillresults'
-        NbaDataProvider.__init__(self, url_modifier=url_modifier, default_params=p_draft)
+        NbaDataProvider.__init__(self, url_modifier=url_modifier, default_params=p_draft, SeasonYear=season)
 
     def data(self):
         return self.object_manager.get_table_from_data(self.object_manager.data_tables, 0)
 
 
 class non_stationary_shooting(NbaDataProvider):
-    def __init__(self):
+    def __init__(self, season=default_season):
         url_modifier = 'draftcombinenonstationaryshooting'
-        NbaDataProvider.__init__(self, url_modifier=url_modifier, default_params=p_draft)
+        NbaDataProvider.__init__(self, url_modifier=url_modifier, default_params=p_draft, SeasonYear=season)
 
     def data(self):
         return self.object_manager.get_table_from_data(self.object_manager.data_tables, 0)
 
 
 class spot_up_shooting(NbaDataProvider):
-    def __init__(self):
+    def __init__(self, season=default_season):
         url_modifier = 'draftcombinespotshooting'
-        NbaDataProvider.__init__(self, url_modifier=url_modifier, default_params=p_draft)
+        NbaDataProvider.__init__(self, url_modifier=url_modifier, default_params=p_draft, SeasonYear=season)
 
     def data(self):
         return self.object_manager.get_table_from_data(self.object_manager.data_tables, 0)
 
 
 class combine(NbaDataProvider):
-    def __init__(self):
+    def __init__(self, season=default_season):
         url_modifier = 'draftcombinestats'
-        NbaDataProvider.__init__(self, url_modifier=url_modifier, default_params=p_draft)
+        NbaDataProvider.__init__(self, url_modifier=url_modifier, default_params=p_draft, SeasonYear=season)
 
     def data(self):
         return self.object_manager.get_table_from_data(self.object_manager.data_tables, 0)
