9378811376c9e26482efc959aadaa01499cbac60
omermazig <omermazig@gmail.com>
2019-11-16 16:21:04 +0200

    Change header data to something I found here:
    https://github.com/swar/nba_api/blob/master/docs/nba_api/stats/examples.md
    
    Don't know why this works (I suspect it's because of the Referer) and the old one doesn't but it's true.

--- goldsberry/masterclass.py
+++ goldsberry/masterclass.py
@@ -13,15 +13,14 @@ install_aliases()
 from urllib.parse import urljoin
 
 header_data = {
-    'Accept-Encoding': 'gzip, deflate, sdch',
-    'Accept-Language': 'en-US,en;q=0.8',
-    'Upgrade-Insecure-Requests': '1',
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:61.0) Gecko/20100101 Firefox/61.0'
-                  'Safari/537.36',
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9'',image/webp,*/*;q=0.8',
-    'Cache-Control': 'max-age=0',
-    'Connection': 'keep-alive'
-}
+        'Host': 'stats.nba.com',
+        'Connection': 'keep-alive',
+        'Accept': 'application/json, text/plain, */*',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36',
+        'Referer': 'https://stats.nba.com/',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Accept-Language': 'en-US,en;q=0.9',
+    }
 
 
 class ObjectManager(object):
